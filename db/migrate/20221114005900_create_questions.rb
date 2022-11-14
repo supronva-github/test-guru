49b61a919e58005dd54e7560a9ec9fc04d3930e1
Evil Potato <vs@MacBook-Pro-Evil.local>
2022-11-14 04:24:17 +0300

    fix:changing migrations for test, category, answer, question models

--- db/migrate/20221114005900_create_questions.rb
+++ db/migrate/20221114005900_create_questions.rb
@@ -1,9 +1,9 @@
 class CreateQuestions < ActiveRecord::Migration[6.1]
   def change
     create_table :questions do |t|
-      t.string :body
-      t.integer :test_id
-
+      t.string :body, null: false
+      t.references :test, null: false, foreign_key: true
+      
       t.timestamps
     end
   end
