ce27368075213bbd910af2e86f1d49e60e295039
Evil Potato <vs@MacBook-Pro-Evil.local>
2022-11-15 23:25:27 +0300

    feat: adding associations to models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  has_many :users_tests
+  has_many :users, through: :users_tests
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :users_tests
+  has_many :tests, through: :users_tests
 end
