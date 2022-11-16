379ab4aafe93e4d589de7b67366649783aed96a8
Evil Potato <vs@MacBook-Pro-Evil.local>
2022-11-16 13:55:09 +0300

    feat: add migration, check constraint for null in table users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_15_193618) do
+ActiveRecord::Schema.define(version: 2022_11_16_104334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
