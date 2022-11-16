f2007ef7e82b2297272ab7e91a3198159a3356c1
Evil Potato <vs@MacBook-Pro-Evil.local>
2022-11-16 13:59:09 +0300

    style: change method name in model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :users_tests
   has_many :tests, through: :users_tests
 
-  def show_all_tests_for_the_selected_level(level)
+  def test_by_level(level)
     tests.where(level: level)
   end
 end
