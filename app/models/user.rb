c68e7f6ddc688e9939604d9b243fc844842800c6
Evil Potato <vs@MacBook-Pro-Evil.local>
2022-11-16 04:19:02 +0300

    feat: add functions to show all user tests for the selected level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :users_tests
   has_many :tests, through: :users_tests
+
+  def show_all_tests_for_the_selected_level(level)
+    tests.where(level: level)
+  end
 end
