b73bd1a3b8c340a4d539eff50e1b71bcac03eef3
Evil Potato <vs@MacBook-Pro-Evil.local>
2022-11-25 02:12:55 +0700

    feat: add validation - test can only be a positive integer

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,7 +6,7 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
   validates :title, presence: true
-
+  validates :level, numericality: {only_integer: true, greater_than_or_equal_to: 0}
   scope :easy, -> {by_level(0..1)}
   scope :medium, -> {by_level(2..4)}
   scope :hard, -> {by_level (5..Float::INFINITY)}
