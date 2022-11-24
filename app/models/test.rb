f089e656926eaf5158c345c97de4addfedd8b250
Evil Potato <vs@MacBook-Pro-Evil.local>
2022-11-24 23:13:33 +0700

    perf: change method test_by_level via scope

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,6 +8,7 @@ class Test < ApplicationRecord
   scope :easy, -> {where(level: 0..1)}
   scope :medium, -> {where(level: 2..4)}
   scope :hard, -> {where(level: 5..Float::INFINITY)}
+  scope :by_level, -> (level) {where(level: level)}
 
   def self.sort_by_category(name)
     joins(:category).where(categories: {title: name}).order(title: :asc).pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :owner_tests, class_name: 'Test', foreign_key: :author_id
 
   def test_by_level(level)
-    tests.where(level: level)
+    tests.by_level(level)
   end
 end
