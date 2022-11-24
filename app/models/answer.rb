72cea1b2dc2fc75c8c95c83865af880cf5ffe42c
Evil Potato <vs@MacBook-Pro-Evil.local>
2022-11-25 02:57:29 +0700

    feat: add validation - max count of answers per question

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,16 @@
 class Answer < ApplicationRecord
+  MAX_COUNT = 4
+
   belongs_to :question
 
   validates :body, presence: true
+  validate :validate_count_in_one_question, on: :create
 
   scope :correct, -> {where(correct: true)}
+
+  private
+
+  def validate_count_in_one_question
+    errors.add(:base, "Уже создано больше 4х ответов на данный вопрос: #{question.body}") if question.answers.count >= MAX_COUNT
+  end
 end
