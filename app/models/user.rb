d82c7b69f907f7ebd69af738510a6f2a94b19a02
Evil Potato <vs@MacBook-Pro-Evil.local>
2022-11-25 05:21:07 +0700

    feat: adding presence validation for first_name and last_name fields in model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :owner_tests, class_name: 'Test', foreign_key: :author_id
 
   validates :email, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
   def test_by_level(level)
     tests.by_level(level)
