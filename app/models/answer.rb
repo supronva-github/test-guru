0f644fa21e501e122835b24d444eaf20dd8448d7
Evil Potato <vs@MacBook-Pro-Evil.local>
2022-11-25 04:32:14 +0700

    style: all models to rubocop style

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,11 +6,14 @@ class Answer < ApplicationRecord
   validates :body, presence: true
   validate :validate_count_in_one_question, on: :create
 
-  scope :correct, -> {where(correct: true)}
+  scope :correct, -> { where(correct: true) }
 
   private
 
   def validate_count_in_one_question
-    errors.add(:base, "Уже создано больше 4х ответов на данный вопрос: #{question.body}") if question.answers.count >= MAX_COUNT
+    if question.answers.count >= MAX_COUNT
+      errors.add(:base,
+                 "Уже создано больше 4х ответов на данный вопрос: #{question.body}")
+    end
   end
 end
