668c5f6aff267fae69ecaed6ce0530a84fea478d
Evil Potato <vs@MacBook-Pro-Evil.local>
2022-11-24 23:21:54 +0700

    perf: change scope, choice of test by difficulty level

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,9 +5,9 @@ class Test < ApplicationRecord
   has_many :users, through: :users_tests
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
-  scope :easy, -> {where(level: 0..1)}
-  scope :medium, -> {where(level: 2..4)}
-  scope :hard, -> {where(level: 5..Float::INFINITY)}
+  scope :easy, -> {by_level(0..1)}
+  scope :medium, -> {by_level(2..4)}
+  scope :hard, -> {by_level (5..Float::INFINITY)}
   scope :by_level, -> (level) {where(level: level)}
 
   def self.sort_by_category(name)
