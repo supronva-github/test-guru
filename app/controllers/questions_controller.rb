fcf61da864660cd97f9e0a83d71887ab519da049
Evil Potato <vs@MacBook-Pro-Evil.local>
2022-12-07 18:42:04 +0300

    feat: add action show for controller questions,view specific question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,14 +1,23 @@
 class QuestionsController < ApplicationController
-  before_action :find_test
+  before_action :find_test, only: [:index]
+  before_action :find_question, only: [:show]
 
   def index
     @questions = @test.questions
     render inline: 'Тесты: <%@questions.each do |q| %><p><%= q.body %></p><% end %>'
   end
 
+  def show
+    render plain: @question.body
+  end
+
   private
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 end
