6af5a608aef83148e3e8c90c53159b9144b390ef
Evil Potato <vs@MacBook-Pro-Evil.local>
2022-12-07 18:55:11 +0300

    feat: exception handling to detect when the question was not found

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,8 @@ class QuestionsController < ApplicationController
   before_action :find_test, only: [:index]
   before_action :find_question, only: [:show]
 
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_question_not_found
+
   def index
     @questions = @test.questions
     render inline: 'Тесты: <%@questions.each do |q| %><p><%= q.body %></p><% end %>'
@@ -19,5 +21,9 @@ class QuestionsController < ApplicationController
 
   def find_question
     @question = Question.find(params[:id])
+  end
+
+  def rescue_question_not_found
+    render plain: 'Вопрос не был найден'
   end
 end
