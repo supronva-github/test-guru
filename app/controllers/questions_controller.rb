f5c6bb7a9f0ff667d0a9a5f8ff4f5d4b21aae160
Evil Potato <vs@MacBook-Pro-Evil.local>
2022-12-10 20:37:03 +0300

    feat: add edit view and action update for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: %i[index create new]
-  before_action :find_question, only: [:show, :destroy]
+  before_action :find_question, only: %i[show destroy edit update]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_question_not_found
 
@@ -18,6 +18,14 @@ class QuestionsController < ApplicationController
       redirect_to @question
     else
       render :new
+    end
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
     end
   end
 
