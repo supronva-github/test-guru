cf8849db8d81a45a84d296d847fe40ccbb71ef22
Evil Potato <vs@MacBook-Pro-Evil.local>
2022-12-12 15:04:01 +0300

    feat: add root rout and action/view index for tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root to: 'tests#index'
+
   resources :tests, only: %i[index show] do
     resources :questions, shallow: true
   end
