9537c9bea1662e515918e0ff1c942ce5cff46760
Evil Potato <vs@MacBook-Pro-Evil.local>
2022-12-15 19:16:21 +0300

    feat: renaming and changing the structure of the users_test table to test_passage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :users_tests
-  has_many :tests, through: :users_tests
+  has_many :test_passages
+  has_many :tests, through: :test_passages
   has_many :owner_tests, class_name: 'Test', foreign_key: :author_id
 
   validates :email, presence: true
