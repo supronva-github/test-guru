a541ac0547237d58a84054e9e662cb99580f6f9b
Evil Potato <vs@MacBook-Pro-Evil.local>
2022-12-16 13:34:07 +0300

    feat: add a scaffold controller and routes for test_passeges

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,11 @@ Rails.application.routes.draw do
       resources :answers, shallow: true
     end
   end
+
+  resources :test_passages, only: %i[show update] do
+    member do
+      get :result
+    end
+  end
 end
+ 
