523dfc970d3e9ade15f754e1884a65cbacea2671
Evil Potato <vs@MacBook-Pro-Evil.local>
2022-12-22 19:58:24 +0300

    feat: add helper, test result output

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,2 +1,17 @@
 module TestPassagesHelper
+  def color_result(test_passage)
+    if test_passage.success?
+      'success'
+    else
+      'fail'
+    end
+  end
+
+  def test_result(test_passage)
+    if test_passage.success?
+      'Тест выполнен'
+    else
+      'Тест не выполнен'
+    end
+  end
 end
