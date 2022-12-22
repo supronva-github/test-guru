25880e0155e093768a4b6b15b601c8c8e5c6b5da
Evil Potato <vs@MacBook-Pro-Evil.local>
2022-12-22 21:32:02 +0300

    refactor: moving rmethod not_found to general class

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,8 +2,6 @@ class AnswersController < ApplicationController
   before_action :find_question, only: %i[new create]
   before_action :find_answer, only: %i[show edit update destroy]
 
-  rescue_from ActiveRecord::RecordNotFound, with: :rescue_answer_not_found
-
   def new
     @answer = @question.answers.new
   end
@@ -31,10 +29,6 @@ class AnswersController < ApplicationController
   end
 
   private
-
-  def rescue_answer_not_found
-    render plain: 'Ответ не найден'
-  end
 
   def find_answer
     @answer = Answer.find(params[:id])
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    rescue_not_found(e)
+  end
+
+  private
+  
+  def rescue_not_found(e)
+    render plain: e.message
+  end
 end
