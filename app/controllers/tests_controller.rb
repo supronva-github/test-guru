05fc47853bed41a50fed485434aaf67d6f929454
Evil Potato <vs@MacBook-Pro-Evil.local>
2022-12-23 18:26:13 +0300

    rafactor: removing the callback associated with finding a test

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,4 @@
 class TestsController < ApplicationController
-  before_action :find_test, only: %i[start]
   before_action :find_user, only: %i[start]
 
   def index
@@ -7,15 +6,12 @@ class TestsController < ApplicationController
   end
 
   def start
+    @test = Test.find(params[:id])
     @user.tests.push(@test)
     redirect_to @user.test_passage(@test)
   end
 
   private
-
-  def find_test
-    @test = Test.find(params[:id])
-  end
 
   def find_user
     @user = User.first
