df744a2d33710a155caae6d87e99e511223504ed
Evil Potato <vs@MacBook-Pro-Evil.local>
2023-01-10 12:23:33 +0300

    feat: add flash message if user not logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to tests_path
     else
+      flash.now[:alert] = 'Verify your Email and Password please'
       render :new
     end
   end
