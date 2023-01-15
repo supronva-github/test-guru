70f5bac079ba4291db04c5dca083c688044941fc
Evil Potato <vs@MacBook-Pro-Evil.local>
2023-01-15 18:14:13 +0300

    feat: add migration, devise to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :owner_tests, class_name: 'Test', foreign_key: :author_id
