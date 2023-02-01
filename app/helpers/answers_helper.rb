96369432e3f0980c07ae516a6e06f99f1bf83fe8
Evil Potato <supronva@gmail.com>
2023-02-01 20:04:15 +0300

    feat: localization answers ru/en

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,9 +1,9 @@
 module AnswersHelper
   def answer_header(answer)
     if answer.new_record?
-      "Create New #{answer.question.body} Answer"
+      I18n.t('helper_methods.answers.new_record', record: answer.question.body)
     else
-      "Edit #{answer.question.body} Answer"
+      I18n.t('helper_methods.answers.not_new_record', record: answer.question.body)
     end
   end
 end
