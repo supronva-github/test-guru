786f39be99f1d9f01178b505fe941b1cb848f0ef
Evil Potato <supronva@gmail.com>
2023-02-02 02:04:16 +0300

    feat: localization test_passages for users ru/en

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,9 +1,9 @@
 module TestPassagesHelper
   def test_result(test_passage)
     if test_passage.success?
-      "<p> Ваш результат: <span class='success'>#{@test_passage.percent_correct_answers}%</span> Тест выполнен </p>".html_safe
+     "<p>#{I18n.t('helper_methods.test_passages.test_success')}<span class='success'>#{@test_passage.percent_correct_answers}%</span></p>".html_safe
     else
-      "<p> Ваш результат: <span class='fail'>#{@test_passage.percent_correct_answers}%</span> Тест не выполнен </p>".html_safe
+      "<p>#{I18n.t('helper_methods.test_passages.test_failed')}<span class='fail'>#{@test_passage.percent_correct_answers}%</span></p>".html_safe
     end
   end
 end
