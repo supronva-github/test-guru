52965716845159d315211c22ac43ed31ef202387
Evil Potato <supronva@gmail.com>
2023-02-08 17:20:52 +0300

    feat: add  bootstrap-icons and create icon helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,8 @@ module ApplicationHelper
       content_tag :p, message, class: "flash #{key}"
     end.join.html_safe
   end
+
+  def icon(name)
+      "<i class='bi bi-#{name}'></i>".html_safe
+  end
 end
