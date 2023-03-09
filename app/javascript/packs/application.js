94e2b8478a8ef6a425ee5a2b43323178422c1a35
Evil Potato <supronva@gmail.com>
2023-03-09 17:34:22 +0300

    feat: validating password confirmation and displaying a notification via JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "utilities/sorting"
+import "utilities/password_check"
 
 Rails.start()
 Turbolinks.start()
