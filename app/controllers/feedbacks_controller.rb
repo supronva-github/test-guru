88494d61a961c33cd88222457e5be913c2f73f2f
Evil Potato <supronva@gmail.com>
2023-04-20 01:36:02 +0300

    feat: add routes and controller feedback

---
+++ app/controllers/feedbacks_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class FeedbacksController < ApplicationController
+  def new
+    @feedback = current_user.feedbacks.new
+  end
+
+  def create
+    @feedback = current_user.feedbacks.new(feedback_params)
+    if @feedback.save
+      redirect_to root_path
+    else
+      render new
+    end
+  end
+
+  private
+
+  def feedback_params
+    params.require(:feedback).permit(:title, :body)
+  end
+end
