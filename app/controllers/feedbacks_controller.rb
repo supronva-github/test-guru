6aef0b53c6d7ff8663b6543e54f0e80ecd2c46cf
Evil Potato <supronva@gmail.com>
2023-04-20 02:58:21 +0300

    feat: add views feedback

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -10,7 +10,7 @@ class FeedbacksController < ApplicationController
     if @feedback.save
       redirect_to root_path
     else
-      render new
+      render :new
     end
   end
 
--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,4 +2,5 @@
 
 class Feedback < ApplicationRecord
   belongs_to :user
+  validates :title, :body, presence: true
 end
